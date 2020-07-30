fbc373ce0ac329dfe0b6d381e74b6aa2838db54f
brian leung <brian@lucky.com.hk>
2020-07-30 16:27:53 +0800

    init

--- Startup.cs
+++ Startup.cs
@@ -11,8 +11,6 @@ namespace SignalRChat
 	{
 		public void Configuration(IAppBuilder app)
 		{
-			
-
 			// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
 			app.MapSignalR();
 		}
